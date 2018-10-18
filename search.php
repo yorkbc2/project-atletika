5d53f84c665ae69e22893c9f3ef5c1bc72e289ce
YegorMamay <web@brainworks.com.ua>
2018-10-18 20:54:30 +0300

    some changes on the search page

--- search.php
+++ search.php
@@ -1,10 +1,12 @@
 <?php get_header(); ?>
 
 <div class="sp-xs-2 sp-sm-2 sp-md-2 sp-lg-2 sp-xl-2"></div>
-
 <h1><?php _e('Search Results for', 'brainworks'); ?> &ldquo;<?php the_search_query(); ?>&rdquo;</h1>
+
 <div class="sp-xs-1 sp-sm-1 sp-md-1 sp-lg-1 sp-xl-1"></div>
 <hr/>
+<div class="sp-xs-1 sp-sm-1 sp-md-1 sp-lg-1 sp-xl-1"></div>
+
 <?php get_template_part('loops/content', 'search'); ?>
 
 <?php get_footer(); ?>
